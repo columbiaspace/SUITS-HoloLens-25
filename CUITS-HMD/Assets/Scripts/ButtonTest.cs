df467d7a26595600723fc98cd691db09065c19a5
angelalbai <113465224+angelalbai@users.noreply.github.com>
2025-02-05 16:10:59 -0500

    Testing push

---
+++ CUITS-HMD/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonTest : MonoBehaviour
+{
+
+    public TextMeshProUGUI numberText; // TextMeshPro object
+    int counter;
+
+    public void ButtonPress()
+    {
+        counter++;
+        // Assign the value of counter to numberText
+        numberText.text = counter + ""; // + "" makes it a string
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
