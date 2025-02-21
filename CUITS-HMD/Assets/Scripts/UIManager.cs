9969cf8cbdcb955645037e71bb56f97c5facef47
hibaa8 <hibaaltaf98@gmail.com>
2025-02-21 17:26:17 -0500

    set up main screen, base functionality

---
+++ CUITS-HMD/Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    // }
+    // Update is called once per frame
+    // void Update()
+    // {
+    // }
+    public GameObject MainMenuObj;
+    public GameObject AnalysisScreenObj;
+    public GameObject ViewDatabaseObj;
+
+    public void hideAllScreens(){
+        MainMenuObj.SetActive(false);
+        AnalysisScreenObj.SetActive(false);
+        ViewDatabaseObj.SetActive(false);
+    }
+
+    public void showMainScreen(){
+        hideAllScreens();
+        AnalysisScreenObj.SetActive(true);
+    }
+
+    public void showDatabaseScreen(){
+        hideAllScreens();
+        ViewDatabaseObj.SetActive(true);
+    }
+
+    public void showRockAnalysisScreen(){
+        hideAllScreens();
+        AnalysisScreenObj.SetActive(true);
+    }
+
+}
