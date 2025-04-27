c4f59bae6a2f596ef0a08044c31e1bb86c9e1b50
hibaa8 <hibaaltaf98@gmail.com>
2025-04-27 19:13:40 -0400

    connected back and frontend. TSS data not renders on UIgit add .

--- CUITS-HMD/Assets/Scripts/UIManager.cs
+++ CUITS-HMD/Assets/Scripts/UIManager.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // if you use Text
+using TMPro; // if you use TextMeshProUGUI
 
 public class UIManager : MonoBehaviour
 {
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    // }
     // Update is called once per frame
     // void Update()
     // {
@@ -15,6 +13,15 @@ public class UIManager : MonoBehaviour
     public GameObject MainMenuObj;
     public GameObject AnalysisScreenObj;
     public GameObject ViewDatabaseObj;
+
+    public TextMeshProUGUI analysisOutputText; // Assign this in the inspector
+    private RockAnalyzer analyzer;
+
+    private void Start()
+    {
+        analyzer = GetComponent<RockAnalyzer>();
+        showMainScreen();
+    }
 
     public void hideAllScreens(){
         MainMenuObj.SetActive(false);
@@ -32,9 +39,39 @@ public class UIManager : MonoBehaviour
         ViewDatabaseObj.SetActive(true);
     }
 
-    public void showRockAnalysisScreen(){
+    public void showRockAnalysisScreen()
+    {
+        Debug.Log("Run Analysis Button Clicked");
+        StartCoroutine(showAnalysisScreenCoroutine()); // <-- StartCoroutine required
+    }
+
+    public IEnumerator showAnalysisScreenCoroutine()
+    {
         hideAllScreens();
+
+        Debug.Log("Starting Rock Analysis...");
+
+        // Fetch rock data
+        var results = analyzer.AnalyzeRock();
+
+        Debug.Log("Finished fetching data from server.");
+
+        yield return new WaitForSeconds(3.0f);
+
+        // Build output string
+        string output = "EVA1 chemical composition:\n";
+        foreach (var kvp in results)
+        {
+            output += $"{kvp.Key}: {kvp.Value}\n";
+            Debug.Log($"{kvp.Key}: {kvp.Value}"); 
+        }
+
+        // Set the text field
+        analysisOutputText.text = output;
+
+        // Now show the analysis screen
         AnalysisScreenObj.SetActive(true);
     }
+
 
 }
