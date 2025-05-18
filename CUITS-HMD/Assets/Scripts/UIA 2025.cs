92d4201173bd28d418772ec5928fb6adb2978fff
Andrew Bonilla <81991875+acbonilla1@users.noreply.github.com>
2025-05-18 17:35:43 -0500

    Beginning UIA work

---
+++ CUITS-HMD/Assets/Scripts/UIA 2025.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+
+public class UIA2025 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
