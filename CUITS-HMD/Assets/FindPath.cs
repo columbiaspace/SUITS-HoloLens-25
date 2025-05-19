f6df249e732fcc56e4b9e84ec3ff6a8b1812be83
Tejas Tirthapura <tirthapuratejas@gmail.com>
2025-05-19 03:42:04 -0500

    idk

--- CUITS-HMD/Assets/FindPath.cs
+++ CUITS-HMD/Assets/FindPath.cs
@@ -2,28 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FindPath : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public Vector2Int start;
-    public int poi;
+public Vector2Int end;
 
-    private list pois = [(poi1), (poi2), (poi3)]; //shira will figure this out
-    
-    
+private Vector2Int start;
 
-    void Start()
-    {
-        request(eva pos)
-        start = modify the eva pos
-        request(compute path(start and end))
-
-        print out path with gpt code 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
---
+++ CUITS-HMD/Assets/PathDrawer.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDrawer : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public float tileSize = 1f; // adjust based on your grid
+
+    public void DrawPath(List<List<int>> path)
+    {
+        if (path == null || path.Count == 0) return;
+
+        lineRenderer.positionCount = path.Count;
+
+        for (int i = 0; i < path.Count; i++)
+        {
+            int y = path[i][0]; // row
+            int x = path[i][1]; // column
+
+            Vector3 worldPos = new Vector3(x * tileSize, 0.1f, y * tileSize); // lifted slightly for visibility
+            lineRenderer.SetPosition(i, worldPos);
+        }
+    }
+}
