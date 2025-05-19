b2386dbeca67fbc356e32d2e803001709814fb0e
Tejas Tirthapura <tirthapuratejas@gmail.com>
2025-05-19 02:37:31 -0500

    added pseudocode to FindPath.cs

---
+++ CUITS-HMD/Assets/FindPath.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2Int start;
+    public int poi;
+
+    private list pois = [(poi1), (poi2), (poi3)]; //shira will figure this out
+    
+    
+
+    void Start()
+    {
+        request(eva pos)
+        start = modify the eva pos
+        request(compute path(start and end))
+
+        print out path with gpt code 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
